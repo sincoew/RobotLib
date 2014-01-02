7488402cfd38ba6fa49ead3bbdb592b7790f6d3a
sincoew <sincoew@yahoo.com.tw>
2014-01-02 18:58:00 +0800

    add UI function

--- RobotLib/DebugUI.Designer.cs
+++ RobotLib/DebugUI.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LogListbox = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Enter_Btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LogListbox
+            // 
+            this.LogListbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LogListbox.FormattingEnabled = true;
+            this.LogListbox.HorizontalExtent = 1000;
+            this.LogListbox.HorizontalScrollbar = true;
+            this.LogListbox.ItemHeight = 12;
+            this.LogListbox.Location = new System.Drawing.Point(0, 0);
+            this.LogListbox.Name = "LogListbox";
+            this.LogListbox.ScrollAlwaysVisible = true;
+            this.LogListbox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.LogListbox.Size = new System.Drawing.Size(284, 219);
+            this.LogListbox.TabIndex = 17;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 219);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 22);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.Text = "Debug Command";
+            // 
+            // Enter_Btn
+            // 
+            this.Enter_Btn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Enter_Btn.Location = new System.Drawing.Point(0, 241);
+            this.Enter_Btn.Name = "Enter_Btn";
+            this.Enter_Btn.Size = new System.Drawing.Size(284, 21);
+            this.Enter_Btn.TabIndex = 15;
+            this.Enter_Btn.Text = "Enter";
+            this.Enter_Btn.UseVisualStyleBackColor = true;
+            this.Enter_Btn.Click += new System.EventHandler(this.Enter_Btn_Click);
+            // 
+            // DebugUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.LogListbox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Enter_Btn);
+            this.Name = "DebugUI";
+            this.Text = "Debug UI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox LogListbox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Enter_Btn;
     }
 }
 
--- RobotLib/DebugUI.cs
+++ RobotLib/DebugUI.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,83 @@ namespace RobotLib
 {
     public partial class DebugUI : Form
     {
+        private const int WM_LBUTTONDOWM = 0x0201;
+        private const int WM_NCLBUTTONDOWM = 0x00A1;
+        private const int HTCAPTION = 2;
+
         public DebugUI()
         {
             InitializeComponent();
         }
+
+        private void DebugUI_Load(object sender, EventArgs e)
+        {
+            this.LogListbox.DrawMode = DrawMode.OwnerDrawFixed;
+            this.LogListbox.DrawItem += OnLogListbox_DrawItem;
+        }
+
+        private void OnLogListbox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            ListBox list = (ListBox)sender;
+            if (list.Items.Count <= 0)
+                return;
+
+            if (LogListbox.Items[e.Index].ToString().Contains("[Error]")) //Set log color
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Red), e.Bounds);
+            }
+            else if (LogListbox.Items[e.Index].ToString().Contains("[Warning]"))
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Orange), e.Bounds);
+            }
+            else if (LogListbox.Items[e.Index].ToString().Contains("[Normal]"))
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.GreenYellow), e.Bounds);
+            }
+            else
+            {
+
+            }
+            e.Graphics.DrawString(list.Items[e.Index].ToString(), this.Font, Brushes.Black, e.Bounds);
+        }
+
+        Mutex _Mut = new Mutex();
+        List<String> LogBuf = new List<string>();
+
+        private void OnLogChange(String Msg)
+        {
+            _Mut.WaitOne();
+
+            LogBuf.Add(Msg);
+            LogBuf.Add("--------");
+            _Mut.ReleaseMutex();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _Mut.WaitOne();
+            foreach (String Temp in LogBuf)
+            {
+                String[] SplitStr = Temp.Split("\r\n".ToArray(), StringSplitOptions.RemoveEmptyEntries);
+                foreach (String SpTemp in SplitStr)
+                {
+                    LogListbox.Items.Add(SpTemp);
+                    LogListbox.SetSelected(LogListbox.Items.Count - 1, true);
+                    LogListbox.SetSelected(LogListbox.Items.Count - 1, false);
+                    StreamWriter sw = new StreamWriter("Temp.txt", true);
+                    sw.WriteLine(Temp);
+                    sw.Close();
+                }
+            }
+            LogBuf.Clear();
+            _Mut.ReleaseMutex();
+        }
+
+        private void Enter_Btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
+
 }
